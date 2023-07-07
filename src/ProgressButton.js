02f3e0e5927b0a0cc66e4aa1afe3930b0569501b
Sarath Chintapatla <sarathch@hpe.com>
2023-07-07 18:54:51 +0530

    Add Timer

--- src/ProgressButton.js
+++ src/ProgressButton.js
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Box, Meter } from 'grommet';
 
 const ProgressButton = () => {
-  const [progress, setProgress] = useState(null);
+  const [progress, setProgress] = useState(20);
+//   const [pollCount, setPollCount] = useState(0);
+    let pollCount = 2
+    var pollInterval;
   const url = "https://mocki.io/v1/221c03e2-312b-4d66-9148-a0d21aa7f79c";
 
   const fetchProgress = () => {
-    // Make API call here to fetch progress
+    // Make API call to fetch initial progress
     // Replace 'apiEndpoint' with your actual API endpoint
-    fetch(url)
-      .then(response => response.json())
-      .then(data => setProgress(data.progress))
+    axios.get(url)
+      .then(response => {
+        setProgress(progress);
+
+        // Start polling the status API every 5 seconds
+        pollInterval = setInterval(pollStatus, 5000);
+      })
       .catch(error => console.error('Error:', error));
+  };
+
+  const pollStatus = () => {
+    // Make API call to fetch status
+    // Replace 'statusEndpoint' with your actual status API endpoint
+    alert(pollCount)
+    if(pollCount < 5 ){
+        pollCount = pollCount + 1;
+           axios.get(url)
+      .then(response => {
+        setProgress(progress + pollCount * 10);
+      })
+      .catch(error => console.error('Error:', error));
+    } else {
+        alert("stopping api");
+        clearInterval(pollInterval);
+    }
+
   };
 
   return (
